36239f58295a2cd81c4e83b74d337e042b53f3fb
EC2 Default User <ec2-user@ip-172-31-61-236.ec2.internal>
2021-05-02 22:52:05 +0000

    [Finish]2-6

---
+++ lesson6.rb
@@ -0,0 +1,20 @@
+total_price=100
+if total_price>=101
+  puts "みかんを購入。所持金に余裕あり。"
+end
+if total_price==100
+  puts "みかんを購入。所持金は０円。"
+end
+if total_price<=99
+  puts "みかんを購入することは出来ません。"
+end
+
+if total_price>100
+  puts "みかんを購入。所持金に余裕あり。"
+
+elsif total_price==100
+   puts "みかんを購入。所持金は０円。"
+
+else
+  puts "みかんを購入することは出来ません。"
+end
